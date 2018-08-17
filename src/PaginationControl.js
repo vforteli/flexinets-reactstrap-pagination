a12b548d5345ab990e187e765eba60ff6369c149
Unknown <verner@fortelius.net>
2018-08-18 00:09:41 +0300

    stuff

--- src/PaginationControl.js
+++ src/PaginationControl.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Pagination, PaginationItem, PaginationLink } from 'reactstrap';
 
-// todo add tests
 
 class PaginationControl extends React.Component {
 
